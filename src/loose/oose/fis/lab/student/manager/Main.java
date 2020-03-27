948b18237e7304170e9f77ad07a42c3b8390f004
Cristian <popacristianpaul48@yahoo.com>
2020-03-27 13:10:42 +0200

    SM-4 Added styling for the view-students scene

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -11,11 +11,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
-        primaryStage.setTitle("Student Manager");
+        primaryStage.setTitle("FIS - Student Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
-primaryStage.setTitle("FIS - Student Manager");
+//primaryStage.setTitle("FIS - Student Manager");
 
 
     public static void main(String[] args) {
